5d983e2011957c373ca829f538c3baaa79266308
Minh Nguyen <1168534+minhnh@users.noreply.github.com>
2025-10-09 23:48:26 +0200

    adapt bhv to allow consuming initial event of a state
    - When there's no steps executed between the start event and end event of
      a state, event reconfiguration must be called both before and after
      stepping the state machine: once before for triggering the state transition
      and once after to trigger the reaction events. Otherwise either the
      steps will be executed twice or the reaction events won't be triggered.
    - This will also remove the on_start logic, as it can be done in the
      state's step function

--- examples/models/fsm/generated_fsm_bhv.py
+++ examples/models/fsm/generated_fsm_bhv.py
@@ -34,19 +34,6 @@ class UserData:
             self.transition_time = self.current_time + self.state_duration
 
 
-def timeout_step(fsm: FSMData, ud: UserData) -> bool:
-    """Return True if timeout has occurred, i.e., state finished."""
-    ud.current_time = time.time()
-    assert ud.transition_time is not None
-    if ud.current_time < ud.transition_time:
-        return False
-
-    # ensure loop period
-    while ud.transition_time < ud.current_time:
-        ud.transition_time += ud.state_duration
-    return True
-
-
 def generic_on_end(fsm: FSMData, ud: UserData, end_events: list[EventID]):
     print(f"State '{StateID(fsm.current_state_index).name}' finished")
     for evt in end_events:
@@ -63,23 +50,31 @@ def idle_on_end(fsm: FSMData, ud: UserData):
     ud.compile = not ud.compile
 
 
-def generic_on_start(fsm: FSMData, ud: UserData, start_event: EventID):
+def generic_step(fsm: FSMData, ud: UserData, start_event: EventID):
+    """Return True if timeout has occurred, i.e., state finished."""
     if consume_event(fsm.event_data, start_event):
         print(f"Entered state '{StateID(fsm.current_state_index).name}'")
+
+    ud.current_time = time.time()
+    assert ud.transition_time is not None
+    if ud.current_time < ud.transition_time:
+        return False
+
+    # ensure loop period
+    while ud.transition_time < ud.current_time:
+        ud.transition_time += ud.state_duration
+    return True
 
 
 def fsm_behavior(fsm: FSMData, ud: UserData, bhv_data: dict):
     cs = fsm.current_state_index
+    # print(f"Current state: '{StateID(fsm.current_state_index).name}'")
     if cs not in bhv_data:
         return
+
     bhv_data_cs = bhv_data[cs]
-    if "on_start" in bhv_data_cs:
-        bhv_data_cs["on_start"](fsm, ud)
-
     assert "step" in bhv_data_cs, f"no step defined for state: {cs}"
-    done = bhv_data_cs["step"](fsm, ud)
-
-    if not done:
+    if not bhv_data_cs["step"](fsm, ud):
         return
 
     if "on_end" in bhv_data_cs:
@@ -93,33 +88,29 @@ def main(state_duration_sec: float):
     fsm = create_fsm()
     fsm_bhv = {
         StateID.S_CONFIGURE: {
-            "on_start": lambda fsm, ud: generic_on_start(
+            "step": lambda fsm, ud: generic_step(
                 fsm, ud, EventID.E_CONFIGURE_ENTERED
             ),
-            "step": timeout_step,
             "on_end": lambda fsm, ud: generic_on_end(
                 fsm, ud, [EventID.E_CONFIGURE_EXIT]
             ),
         },
         StateID.S_IDLE: {
-            "on_start": lambda fsm, ud: generic_on_start(
+            "step": lambda fsm, ud: generic_step(
                 fsm, ud, EventID.E_IDLE_ENTERED
             ),
-            "step": timeout_step,
             "on_end": idle_on_end,
         },
         StateID.S_COMPILE: {
-            "on_start": lambda fsm, ud: generic_on_start(
+            "step": lambda fsm, ud: generic_step(
                 fsm, ud, EventID.E_COMPILE_ENTERED
             ),
-            "step": timeout_step,
             "on_end": lambda fsm, ud: generic_on_end(fsm, ud, [EventID.E_COMPILE_EXIT]),
         },
         StateID.S_EXECUTE: {
-            "on_start": lambda fsm, ud: generic_on_start(
+            "step": lambda fsm, ud: generic_step(
                 fsm, ud, EventID.E_EXECUTE_ENTERED
             ),
-            "step": timeout_step,
             "on_end": lambda fsm, ud: generic_on_end(fsm, ud, [EventID.E_EXECUTE_EXIT]),
         },
     }
@@ -130,11 +121,14 @@ def main(state_duration_sec: float):
         if fsm.current_state_index == StateID.S_EXIT:
             print("State machine completed successfully")
             break
-
+        # Behaviours
         produce_event(fsm.event_data, EventID.E_STEP)
 
         fsm_behavior(fsm, ud, fsm_bhv)
 
+        reconfig_event_buffers(fsm.event_data)
+
+        # State transitions
         fsm_step(fsm)
 
         reconfig_event_buffers(fsm.event_data)
