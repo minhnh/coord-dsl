f53ea016e020b9b2e40a804d0b843e14efe20fa1
Minh Nguyen <minh.nguyen@uni-bremen.de>
2026-03-17 22:46:33 +0100

    generate heap-backed FSM with create/destroy funcs
    * Avoid declaring FSM structs as global variable in generated hpp file.
      Instead, generate create_fsm() as an owning allocator and adding
      destroy_fsm() for cleanup.
    * Update generated usage to pointer-based FSM access.

--- examples/models/fsm/test_fsm.cpp
+++ examples/models/fsm/test_fsm.cpp
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <csignal>
 #include <iostream>
 #include <ostream>
 #include <thread>
@@ -6,49 +7,63 @@
 #include "coord2b/types/fsm.h"
 #include "example.fsm.hpp"
 
+sig_atomic_t stopFlag = 0;
+
+void
+handler(int) {
+    stopFlag = 1;
+}
+
 
 int main() {
     using clock = std::chrono::steady_clock;
+
+    signal(SIGINT, &handler);
+
     constexpr auto tick_period = std::chrono::microseconds(1000);  // 1kHz
     constexpr auto print_period = std::chrono::milliseconds(500);  // 2Hz
     auto next_tick = clock::now();
     auto now = clock::now();
     auto next_print = now + print_period;
+
+    auto fsm = create_fsm();
     bool compile = true;
-    while (true) {
+    while (!stopFlag) {
         next_tick += tick_period;
 
-        produce_event(fsm.eventData, E_STEP);
+        produce_event(fsm->eventData, E_STEP);
 
         // run state machine, event loop
-        fsm_step_nbx(&fsm);
-        reconfig_event_buffers(&eventData);
+        fsm_step_nbx(fsm);
+        reconfig_event_buffers(fsm->eventData);
 
         // handle print
         now = clock::now();
         if (now > next_print) {
             next_print += print_period;
-            std::cout << "State: " << fsm.states[fsm.currentStateIndex].name << std::endl;
+            std::cout << "State: " << fsm->states[fsm->currentStateIndex].name << std::endl;
 
             // simple inline behaviour to go through all states
-            if (fsm.currentStateIndex == S_CONFIGURE) {
-                produce_event(fsm.eventData, E_CONFIGURE_EXIT);
-            } else if (fsm.currentStateIndex == S_IDLE) {
+            if (fsm->currentStateIndex == S_CONFIGURE) {
+                produce_event(fsm->eventData, E_CONFIGURE_EXIT);
+            } else if (fsm->currentStateIndex == S_IDLE) {
                 if (compile) {
-                    produce_event(fsm.eventData, E_IDLE_EXIT_COMPILE);
+                    produce_event(fsm->eventData, E_IDLE_EXIT_COMPILE);
                 } else {
-                    produce_event(fsm.eventData, E_IDLE_EXIT_EXECUTE);
+                    produce_event(fsm->eventData, E_IDLE_EXIT_EXECUTE);
                 }
                 compile = !compile;
-            } else if (fsm.currentStateIndex == S_COMPILE) {
-                produce_event(fsm.eventData, E_COMPILE_EXIT);
-            } else if (fsm.currentStateIndex == S_EXECUTE) {
-                produce_event(fsm.eventData, E_EXECUTE_EXIT);
+            } else if (fsm->currentStateIndex == S_COMPILE) {
+                produce_event(fsm->eventData, E_COMPILE_EXIT);
+            } else if (fsm->currentStateIndex == S_EXECUTE) {
+                produce_event(fsm->eventData, E_EXECUTE_EXIT);
             }
         }
 
         std::this_thread::sleep_until(next_tick);
     }
+
+    destroy_fsm(fsm);
 
     return 0;
 }
